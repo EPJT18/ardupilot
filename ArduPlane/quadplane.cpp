ddea1a71a080459e5bc31c134d17cb9021be6842
Josh <joshua.tepper@outlook.com>
2019-09-22 16:44:34 +1000

    Yaw before transition

--- ArduPlane/quadplane.cpp
+++ ArduPlane/quadplane.cpp
@@ -2673,6 +2673,8 @@ bool QuadPlane::verify_vtol_takeoff(const AP_Mission::Mission_Command &cmd)
 
     // reset takeoff start time if we aren't armed, as we won't have made any progress
     if (!hal.util->get_soft_armed()) {
+
+    }
         takeoff_start_time_ms = now;
     }
         
