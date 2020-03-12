53da42fd69c536f3b979343683a1723c94f2f3ce
Josh <joshua.tepper@outlook.com>
2020-03-12 19:49:19 +1100

    Arming Check- All GPS

--- libraries/AP_GPS/AP_GPS.h
+++ libraries/AP_GPS/AP_GPS.h
@@ -426,7 +426,9 @@ public:
     // report if the gps is healthy (this is defined as existing, an update at a rate greater than 4Hz,
     // as well as any driver specific behaviour)
     bool is_healthy(uint8_t instance) const;
+    bool all_healthy(void) const;
     bool is_healthy(void) const { return is_healthy(primary_instance); }
+
 
     // returns true if all GPS instances have passed all final arming checks/state changes
     bool prepare_for_arming(void);
